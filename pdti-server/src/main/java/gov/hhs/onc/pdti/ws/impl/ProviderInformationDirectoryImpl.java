1f6a7e8e24711a17dd5652fefb969e017cd26a1b
Michal Kotelba <michal.kotelba@esacinc.com>
2013-04-29 08:51:41 -0400

    - Added Log4j configuration file. - Moved XSD + WSDL web resource copying to Maven WAR plugin (webResources). - Added explicit Spring configuration location servlet initialization parameter. - Modified WSDL to use Spring-friendly SOAP v1.2 namespace prefix.

--- pdti-server/src/main/java/gov/hhs/onc/pdti/ws/impl/ProviderInformationDirectoryImpl.java
+++ pdti-server/src/main/java/gov/hhs/onc/pdti/ws/impl/ProviderInformationDirectoryImpl.java
@@ -1,15 +1,15 @@
 package gov.hhs.onc.pdti.ws.impl;
 
+import gov.hhs.onc.pdti.ws.api.BatchRequest;
+import gov.hhs.onc.pdti.ws.api.BatchResponse;
+import gov.hhs.onc.pdti.ws.api.ProviderInformationDirectoryPortType;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import org.apache.log4j.Logger;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import gov.hhs.onc.pdti.ws.api.BatchRequest;
-import gov.hhs.onc.pdti.ws.api.BatchResponse;
-import gov.hhs.onc.pdti.ws.api.ProviderInformationDirectoryPortType;
-
-public class ProviderInformationDirectoryImpl implements ProviderInformationDirectoryPortType {
+public class ProviderInformationDirectoryImpl extends SpringBeanAutowiringSupport implements ProviderInformationDirectoryPortType {
 
 	private static final Logger LOGGER = Logger.getLogger(ProviderInformationDirectoryImpl.class.getName());
 	
@@ -22,8 +22,8 @@ public class ProviderInformationDirectoryImpl implements ProviderInformationDire
 	@WebResult(name = "batchResponse", targetNamespace = "urn:oasis:names:tc:DSML:2:0:core", partName = "body")
 	public BatchResponse providerInformationQueryRequest(
 			@WebParam(name = "batchRequest", targetNamespace = "urn:oasis:names:tc:DSML:2:0:core", partName = "body") BatchRequest body) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new BatchResponse();
 	}
 
 	@Override
@@ -31,8 +31,8 @@ public class ProviderInformationDirectoryImpl implements ProviderInformationDire
 	@WebResult(name = "batchResponse", targetNamespace = "urn:oasis:names:tc:DSML:2:0:core", partName = "body")
 	public BatchResponse providerInformationFeedRequest(
 			@WebParam(name = "batchRequest", targetNamespace = "urn:oasis:names:tc:DSML:2:0:core", partName = "body") BatchRequest body) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new BatchResponse();
 	}
 
 }
