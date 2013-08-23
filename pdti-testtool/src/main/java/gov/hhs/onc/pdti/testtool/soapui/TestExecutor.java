695bc22c9d0aae626a07624a2f3ee8cb870a9a47
Michal Kotelba <michal.kotelba@esacinc.com>
2013-08-23 11:24:52 -0400

    - Fixes MSPDTI-98. - Improved logging in pdti-testtool by using Slf4j (instead of Log4j directly), using a Log4j XML configuration instead of properties, and appending to a log file specified by system properties. - Fixed/added Slf4j and Log4j Maven dependencies. - Fixed javax.servlet.servlet-api dependency (scope needed to be explicitly set to provided). - Improved Maven project by adding dependencies on pdti-ws and pdti-server's test JAR (inc. unpacking the SoapUI project files from the dependency instead of copying resources).

--- pdti-testtool/src/main/java/gov/hhs/onc/pdti/testtool/soapui/TestExecutor.java
+++ pdti-testtool/src/main/java/gov/hhs/onc/pdti/testtool/soapui/TestExecutor.java
@@ -2,30 +2,29 @@ package gov.hhs.onc.pdti.testtool.soapui;
 
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-
 import com.eviware.soapui.impl.wsdl.teststeps.WsdlTestStepResult;
 import com.eviware.soapui.model.testsuite.TestAssertion;
 import com.eviware.soapui.model.testsuite.TestStepResult.TestStepStatus;
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestExecutor {
     
-    private static final Logger LOGGER = Logger.getLogger(TestExecutor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestExecutor.class);
     private static final String SOAPUI_PROJECT_FILE = "soapui-project_hpdplus.xml";
     private static final String URL_PROPERTY = "project.test.server.wsdl.url=";
     private static final String BASE_DN_PROPERTY = "project.test.server.dsml.dn.base=";
     
     public String executeTests(final String wsdlUrlToTest, final String baseDn) {
-        LOGGER.info("DOUG!!!");
-        LOGGER.info("wsdlUrlToTest =" + wsdlUrlToTest + "=");
+        LOGGER.info("wsdlUrlToTest = " + wsdlUrlToTest);
         SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
         runner.setProjectFile(TestExecutor.class.getClassLoader().getResource(SOAPUI_PROJECT_FILE).toString());
         runner.setProjectProperties(new String[] { URL_PROPERTY + wsdlUrlToTest, BASE_DN_PROPERTY + baseDn});
         try {
             runner.run();
         } catch(Exception exception) {
-            LOGGER.error(exception.getMessage());
+            LOGGER.error("Unable to run SoapUI test case(s).", exception);
         }
         StringBuffer stringBuffer = new StringBuffer();
         Map<TestAssertion, WsdlTestStepResult> assertionResultsMap = runner.getAssertionResults();
@@ -33,7 +32,7 @@ public class TestExecutor {
             String assertionName = testAssertion.getName();
             WsdlTestStepResult wsdlTestStepResult = assertionResultsMap.get(testAssertion);
             TestStepStatus testStepStatus = wsdlTestStepResult.getStatus();
-            stringBuffer.append("assertion name =" + assertionName + "=, testStepStatus =" + testStepStatus + "=\n");
+            stringBuffer.append("assertion name = " + assertionName + "=, testStepStatus = " + testStepStatus + "\n");
         }
         return stringBuffer.toString();
     }
